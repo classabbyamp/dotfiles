6db1a79c58f1ab0a791b5e1ca5a6d33dcdd1e5ca
classabbyamp <dev@kb6.ee>
2022-02-28 19:29:22 -0500

    updats for chm 2.13

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -6,6 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+export PATH="$HOME/.local/bin:$PATH"
+
 if [ -z "$1" ]
 then
     # Launch bar1 and bar2
