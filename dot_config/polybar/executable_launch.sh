4772e09725414a2819f9bd0c8317d44302d1dda5
classabbyamp <dev@placeviolette.net>
2022-04-11 14:55:57 -0400

    update polybar launch

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -8,15 +8,15 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 export PATH="$HOME/.local/bin:$PATH"
 
-polybar dp0 &
-polybar dp2 &
-polybar dp1 &
+polybar -r dp0 &
+polybar -r dp2 &
+polybar -r dp1 &
 
 for m in $( bspc query -M --names ); do
     index=$((index + 1))
     export P_BSPWM_WINDOW_CMD="tail $HOME/.cache/bspwm_windows_${index}.txt"
 
-    MONITOR=$m polybar --reload wintitles &
+    MONITOR=$m polybar -r wintitles &
 done
 
 echo "Bars launched..."
