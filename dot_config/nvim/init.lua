0b603baef44592ed2434df4b1781202a72043869
classabbyamp <dev@placeviolette.net>
2023-04-08 04:10:13 -0400

    nvim: update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -271,19 +271,6 @@ require('gitsigns').setup {
 -- end gitsigns }}}
 
 -- treesitter {{{
-local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
-parser_config.typst = {
-  install_info = {
-    url = 'https://github.com/SeniorMars/tree-sitter-typst', -- local path or git repo
-    files = {'src/parser.c', 'src/scanner.c'},
-    -- optional entries:
-    branch = 'main', -- default branch in case of git repo if different from master
-    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-  },
-  filetype = 'typst', -- if filetype does not match the parser name
-}
-
 require('nvim-treesitter.configs').setup({
     ensure_installed = "all",
     sync_install = false,
@@ -299,6 +286,19 @@ require('nvim-treesitter.configs').setup({
         enable = true
     },
 })
+
+-- local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
+-- parser_config.typst = {
+--   install_info = {
+--     url = 'https://github.com/SeniorMars/tree-sitter-typst', -- local path or git repo
+--     files = {'src/parser.c', 'src/scanner.c'},
+--     -- optional entries:
+--     branch = 'main', -- default branch in case of git repo if different from master
+--     generate_requires_npm = true, -- if stand-alone parser without npm dependencies
+--     requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
+--   },
+--   -- filetype = 'typst', -- if filetype does not match the parser name
+-- }
 
 cmd[[ hi commentTSWarning cterm=bold ctermfg=14 gui=bold guifg=Orange ]]
 -- end treesitter }}}
@@ -442,10 +442,7 @@ require('lspconfig').bashls.setup {
     filetypes = { "bash" },
 }
 -- xi typst-lsp
-require('lspconfig').typst_lsp.setup{
-    capabilities = capabilities,
-    filetypes = { "typst" },
-}
+require('lspconfig').typst_lsp.setup{}
 
 -- end completion-lsp }}}
 
