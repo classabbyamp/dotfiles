11221c9452e70efd09748852b6e904e4635b57f0
classabbyamp <dev@placeviolette.net>
2024-03-21 03:36:15 -0400

    nvim: don't dim inactive panes
    it looks weird with the new terminal theme

--- dot_config/nvim/lua/cfg/colorscheme.lua
+++ dot_config/nvim/lua/cfg/colorscheme.lua
@@ -4,11 +4,6 @@ return function()
     require('catppuccin').setup {
         flavour = 'mocha',
         transparent_background = true,
-        dim_inactive = {
-            enabled = true,
-            shade = 'dark',
-            percentage = 0.15,
-        },
         integrations = {
             cmp = true,
             gitsigns = true,
