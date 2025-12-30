d99b04be33caef3b2e5005f765ae2502b661edcc
classabbyamp <dev@placeviolette.net>
2025-12-29 23:39:27 -0500

    nvim: fix virtual text/popups

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -128,6 +128,7 @@ vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, {})
 vim.filetype.add({
     extension = {
         typ = 'typst',
+        typst = 'typst',
         nomad = 'hcl',
         tf = 'hcl',
         bats = 'bash',
@@ -161,13 +162,62 @@ autocmd({'BufWritePre', 'FileWritePre'}, {
     end
 })
 
--- borders for floating windows
-local float_border = { border = 'single' }
+-- config for floating windows
+o.winborder = "single"
+
+local hover_cfg = {
+    border = "single",
+    max_height = 30,
+    max_width = 80,
+}
+
+vim.lsp.buf.hover(hover_cfg)
+vim.lsp.buf.signature_help(hover_cfg)
 
 vim.diagnostic.config({
-    float = float_border,
+    float = { border = o.winborder },
     virtual_text = true,
     virtual_lines = { current_line = true },
+})
+
+-- show diagnostics in expanded format only on the current line
+-- and at the end for other lines
+vim.api.nvim_create_autocmd({ 'CursorMoved', 'DiagnosticChanged' }, {
+     group = vim.api.nvim_create_augroup('diagnostic_virt_text_hide', {}),
+  callback = function(ev)
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local lnum = cursor_pos[1] - 1
+
+   local hidden_lnum = vim.b[ev.buf].diagnostic_hidden_lnum
+   if hidden_lnum and hidden_lnum ~= lnum then
+     vim.b[ev.buf].diagnostic_hidden_lnum = nil
+     -- display all the decorations if the current line changed
+     vim.diagnostic.show(nil, ev.buf)
+   end
+
+   for _, namespace in pairs(vim.diagnostic.get_namespaces()) do
+     local ns_id = namespace.user_data.virt_text_ns
+       if ns_id then
+         local extmarks = vim.api.nvim_buf_get_extmarks(ev.buf, ns_id, { lnum, 0 }, { lnum, -1 }, {})
+         for _, extmark in pairs(extmarks) do
+           local id = extmark[1]
+           vim.api.nvim_buf_del_extmark(ev.buf, ns_id, id)
+         end
+
+         if extmarks and not vim.b[ev.buf].diagnostic_hidden_lnum then
+           vim.b[ev.buf].diagnostic_hidden_lnum = lnum
+        end
+      end
+    end
+  end,
+})
+
+-- redraw diagnostics on mode change
+vim.api.nvim_create_autocmd('ModeChanged', {
+  group = vim.api.nvim_create_augroup('diagnostic_redraw', {}),
+  callback = function()
+    pcall(vim.diagnostic.show)
+  end
 })
 
 if g.neovide then
